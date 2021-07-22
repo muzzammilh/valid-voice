f8c7bb9f93d56b3c1b1c937186694872cdd6aab9
Muzzammil Hussain <muzzammilz@gmail.com>
2021-07-22 19:59:13 +0500

    Removed run.py and simply start using main.py

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import os
 from flask import Flask
 from flask import render_template
 
-from config import config
+from dotenv import load_dotenv
 
+from config import config
 from views import test
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 
 def create_app(config_name):
@@ -19,3 +24,8 @@ def create_app(config_name):
         return os.getenv('FLASK_CONFIG')
 
     return app
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'development')
+
+if __name__ == '__main__':
+    app.run()
